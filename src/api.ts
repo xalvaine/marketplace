afe42809d028b07e6f15fc9b81422053f4fa3e06
Timofey Poluhin <t.poluhin@greenleaf.team>
2021-03-22 20:25:33 +0300

    feature: configure docker and nginx

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: `http://178.154.232.196:21800/api/v1` })
+const api = axios.create({ baseURL: `http://face.demo.greenleaf.team/api/v1` })
 
 export default api
