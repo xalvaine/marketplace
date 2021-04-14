94d054ceb58ee73d8c5d4fffa252f10fc1c8eff4
Timofey Poluhin <t.poluhin@greenleaf.team>
2021-04-14 13:16:18 +0300

    feature: migrate to webpack 5

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  webpack(config) {
+  future: {
+    webpack5: true,
+  },
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
       use: [`@svgr/webpack`],
     })
 
