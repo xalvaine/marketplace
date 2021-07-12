b122a2e8b14958a19f8bc42d02d6f1e977bc9b83
Timofey Poluhin <t.poluhin@greenleaf.team>
2021-07-12 15:40:13 +0300

    chore: update README.md

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 #
 # a simple deploy script
 #
-base="store-frontend" # force directory name to avoid duplicates
+base=$(basename $PWD) # force directory name to avoid duplicates
 
 yarn build
 
 echo Creating the archive...
 tar -czf $base.tar.gz .next nginx dockerfile docker-compose.yml .env yarn.lock node_modules
-rsync --archive --verbose --progress $base.tar.gz dev@178.154.232.196:~/face/$base
+rsync --archive --verbose --progress $base.tar.gz dev@178.154.232.196:~/face/store-frontend
 rm $base.tar.gz -f
 
 ssh dev@178.154.232.196 "docker system prune -f
